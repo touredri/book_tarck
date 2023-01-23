766f8558b2149abbeac74e759a8b18de49dde802
touredri <dcompte223@gmail.com>
2023-01-23 01:28:03 +0100

    add button action to new book button

---
+++ script.js
@@ -0,0 +1,18 @@
+
+var add = document.getElementById("add_book");
+pop = document.getElementById("popup");
+
+add.addEventListener('click', function() {
+    document.getElementById("popup").classList.toggle("display");
+})
+
+
+let myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
